081c90ac2d7b8cb98dcf14be8a9c2d605121d2da
Paan-00 <106901574+Paan-00@users.noreply.github.com>
2024-05-21 16:48:12 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -75,7 +75,7 @@ elif app_mode == "Disease Recognition":
             st.write("Our Prediction")
             result_index = model_prediction(input_image)
             if result_index is not None:
-                class_name = ['Acne', 'Actinic Keratosis', 'Eczema', 'Melanoma', 'Normal', 'Rosacea']
+                class_name = ['Acne', 'Eczema', 'Melanoma', 'Normal']
                 model_predicted = class_name[result_index]
                 st.success(f"Model is Predicting it's {model_predicted}")
             else:
