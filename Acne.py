2c40cf8c8e71a7f1981af1ec12955163c524bf02
Paan-00 <106901574+Paan-00@users.noreply.github.com>
2024-06-04 13:03:29 +0800

    Add files via upload

---
+++ Acne.py
@@ -0,0 +1,20 @@
+st.header("ACNE")
+	
+    st.markdown("""
+    Welcome to the Skin Disease Detection System! 🔍
+    
+    Our mission is to help in identifying skin diseases efficiently. Scan the surface of skin, and our system will analyze it to detect any signs of diseases. Together, let's protect our skin and ensure a healthier body!
+
+    ### How It Works
+    1. **Scan:** Go to the **Disease Recognition** page and scan the surface of skin with suspected diseases.
+    2. **Analysis:** Our system will process the image using advanced algorithms to identify potential diseases.
+    3. **Results:** View the results and recommendations for further action.
+
+    ### Why Choose Us?
+    - **Accuracy:** Our system utilizes state-of-the-art machine learning techniques for accurate disease detection.
+    - **User-Friendly:** Simple and intuitive interface for seamless user experience.
+    - **Fast and Efficient:** Receive results in seconds, allowing for quick decision-making.
+
+    ### Get Started
+    Click on the **Disease Recognition** page in the sidebar to upload an image and experience the power of our Skin Disease Recognition System!
+    """)
